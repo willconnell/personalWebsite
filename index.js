999300cceda93295487d88e7bf086128641b4b1e
Will Connell <willconnell@utexas.edu>
2021-06-01 15:14:28 -0500

    Redesigned site styling & added a project

--- index.js
+++ index.js
@@ -8,20 +8,15 @@ function closeNav() {
     document.getElementById("main").style.marginRight = "0";
 }
 
-
 function showJobA(id) {
     if (id == "jobA") {
-        document.getElementById("jobDescription").innerHTML = "This is the job description for job A";
+        document.getElementById("jobDescription").innerHTML =
+            "This is the job description for job A";
     } else if (id == "clj") {
-        document.getElementById("jobDescription").innerHTML = "This is the job description for clj";
-    } else if (id = "venetian") {
-        document.getElementById("jobDescription").innerHTML = "<ul><li>Coordinated carpet deliveries and exports with truck drivers and installers</li><li>Consulted with business owner in order to reduce warehouse loading times by 10%</li><li>Operated heavy machinery including forklifts to organize the carpet warehouse</li></ul>";
+        document.getElementById("jobDescription").innerHTML =
+            "This is the job description for clj";
+    } else if ((id = "venetian")) {
+        document.getElementById("jobDescription").innerHTML =
+            "<ul><li>Coordinated carpet deliveries and exports with truck drivers and installers</li><li>Consulted with business owner in order to reduce warehouse loading times by 10%</li><li>Operated heavy machinery including forklifts to organize the carpet warehouse</li></ul>";
     }
-    
-
-
-
-
-
-
 }
